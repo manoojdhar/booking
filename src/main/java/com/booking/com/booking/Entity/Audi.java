817a9865ae64075b7a7c135afa1d2da54105feb9
Manooj <manoojkumar.dhar@centific.com>
2025-09-14 00:10:15 +0530

    updated features

--- src/main/java/com/booking/com/booking/Entity/Audi.java
+++ src/main/java/com/booking/com/booking/Entity/Audi.java
@@ -1,7 +1,6 @@
 package com.booking.com.booking.Entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
